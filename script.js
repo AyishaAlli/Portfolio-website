305a37cfc6849cee9057a4398825cac826f98e61
Ayisha <alli.ayisha1@gmail.com>
2021-02-10 20:51:02 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+// Variables  
+
+const barsIcon = document.getElementById('bars-icon');
+const navBar = document.getElementById('nav-links');
+
+// Event Listeners 
+barsIcon.addEventListener('click', function an() {
+   navBar.classList.toggle('show')
+})
+
